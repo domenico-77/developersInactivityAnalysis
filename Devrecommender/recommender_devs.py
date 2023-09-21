896193eed7e523d410d07517ab75ee9d68136ffe
Domenico Vicenti <79905785+domenico-77@users.noreply.github.com>
2023-09-21 02:29:22 +0200

    Update recommender_devs.py

--- Devrecommender/recommender_devs.py
+++ Devrecommender/recommender_devs.py
@@ -5,9 +5,8 @@ if __name__ == "__main__":
     arguments = sys.argv[1:]
     owner = arguments[0]
     repository = arguments[1]
-    token = arguments[2]
-    k = arguments[3]
-    decimal_places = arguments[4]
-    recommender = dev_recommender.Dev_recomender(owner, repository, token)
+    k = arguments[2]
+    decimal_places = arguments[3]
+    recommender = dev_recommender.Dev_recomender(owner, repository, '')
 
     recommender.recomender_substitutes(k, decimal_places)
