c7958d5c8348fd57f644d371c61452b2f35047d0
MaxT-21 <79905859+MaxT-21@users.noreply.github.com>
2023-09-22 16:42:51 +0200

    Update recommender_devs.py

--- Devrecommender/recommender_devs.py
+++ Devrecommender/recommender_devs.py
@@ -5,8 +5,8 @@ if __name__ == "__main__":
     arguments = sys.argv[1:]
     owner = arguments[0]
     repository = arguments[1]
-    k = arguments[2]
-    decimal_places = arguments[3]
+    k = int(arguments[2])
+    decimal_places = int(arguments[3])
     recommender = dev_recommender.Dev_recomender(owner, repository, '')
 
     recommender.recomender_substitutes(k, decimal_places)
